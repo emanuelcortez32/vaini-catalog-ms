3af553344db4086d4cddad7a5befd8c6894bb021
Ema Cortez <emanuelcortez32@gmail.com>
2023-01-12 01:48:18 -0300

    unless

--- src/helpers/creatorResponse.ts
+++ src/helpers/creatorResponse.ts
@@ -7,7 +7,6 @@ type ResponseNoOK = {
     statusCode: 400 | 401 | 403 | 404 | 409 | 500,
     code: string,
     message?: string,
-    errors?: any
 }
 
 export const createResponseOk = (data?: any): ResponseOK => ({ statusCode: 200, data });
@@ -16,35 +15,29 @@ export const createErrorResponseBadRequest = (err?: Error): ResponseNoOK => ({
     statusCode: 400,
     code: 'BAD_REQUEST',
     message: err?.message || "",
-    errors: err?.stack || ""
 })
 export const createErrorResponseUnAuthorized = (err?: Error): ResponseNoOK => ({
     statusCode: 401,
     code: 'NO_AUTHORIZED',
     message: err?.message || "",
-    errors: err?.stack || ""
 }) 
 export const createErrorResponseForbidden = (err?: Error): ResponseNoOK => ({
     statusCode: 403,
     code: 'FORBIDDEN',
     message: err?.message || "",
-    errors: err?.stack || ""
 })
 export const createErrorResponseNotFound = (err?: Error): ResponseNoOK => ({
     statusCode: 404,
     code: 'NOT_FOUND',
     message: err?.message || "",
-    errors: err?.stack || ""
 })
 export const createErrorResponseConflict = (err?: Error): ResponseNoOK => ({
     statusCode: 409,
     code: 'CONFLICT',
     message: err?.message || "",
-    errors: err?.stack || ""
 })
 export const createErrorResponseGeneric = (err?: Error): ResponseNoOK => ({
     statusCode: 500,
     code: 'GENERIC_ERROR',
     message: err?.message || "",
-    errors: err?.stack || ""
 })
